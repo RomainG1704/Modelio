9f91ae3a95bcff54e1fa5606f4f3d6185f1c5b12
Benjamin FOUILHAC <bfouilhac@gmail.com>
2025-01-29 15:24:05 +0100

    Remove d'une variable client useless dans LocationTest.java

--- src/LocationTest.java
+++ src/LocationTest.java
@@ -1,18 +1,15 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LocationTest {
 
-    private Client client;
     private Location location;
     private Film filmtest;
 
     @BeforeEach
     void setUp() {
-        client = new Client("John Doe");
         filmtest = new Film("Film Test", Film.NORMAL);
         location = new Location(filmtest, 5);
     }
